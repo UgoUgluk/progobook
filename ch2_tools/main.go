1c15df00ff2bd3852ad6c833f856257e87202ff8
Oleg Vashkevich <ugluk@tut.by>
2023-03-11 23:05:24 +0300

    ch3 final

--- ch2_tools/main.go
+++ ch2_tools/main.go
@@ -4,17 +4,18 @@ import "fmt"
 
 func main() {
 	PrintHello()
-	for i := 0; i < 5; i++ {
-		PrintNumber(i)
+	for i := 0; i < 5; i++ { // loop with a counter
+		PrintHello()   // print out a message
+		PrintNumber(i) // print out the counter
 	}
 }
 
-//PrintHello out Hello message
+// PrintHello out Hello message
 func PrintHello() {
 	fmt.Println("Hello, Go")
 }
 
-//PrintNumber out Number message
+// PrintNumber out Number message
 func PrintNumber(number int) {
 	fmt.Println(number)
 }
