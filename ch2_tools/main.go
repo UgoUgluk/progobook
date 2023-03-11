fc735ba31812583fe24d26ba2146bb133bddb294
Oleg Vashkevich <ugluk@tut.by>
2023-03-11 19:44:21 +0300

    ch2 revive

---
+++ ch2_tools/main.go
@@ -0,0 +1,24 @@
+// package ch2_tools
+package main
+
+import "fmt"
+
+func main() {
+	PrintHello()
+	for i := 0; i < 5; i++ {
+		PrintNumber(i)
+	}
+}
+
+// revive:disable:exported
+
+func PrintHello() {
+	fmt.Println("Hello, Go")
+}
+
+// revive:enable:exported
+
+// PrintNumber writes a number using the fmt.Println function
+func PrintNumber(number int) {
+	fmt.Println(number)
+}
