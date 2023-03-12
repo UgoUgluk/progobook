f4065f7c293bbc20defcd0283ae78fdc47ea9e94
Oleg Vashkevich <ugluk@tut.by>
2023-03-12 13:30:10 +0300

    ch7 start arrays

---
+++ ch7_collections/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Collections")
+
+	//arrays
+	var names [3]string
+	names[0] = "Kayak"
+	names[1] = "Lifejacket"
+	names[2] = "Paddle"
+	fmt.Println(names)
+
+}
