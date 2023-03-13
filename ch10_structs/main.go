0e3d6c18f0a6349c7abb67ab077baa2dfccd53f8
Oleg Vashkevich <ugluk@tut.by>
2023-03-13 14:11:53 +0300

    ch10: struct builder

--- ch10_structs/main.go
+++ ch10_structs/main.go
@@ -1,66 +1,88 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func writeName(val struct {
+// Product represents a product
+type Product struct {
 	name, category string
 	price          float64
-}) {
-	fmt.Println("Name:", val.name)
+	//otherNames []string
+}
+
+func calcTax(product *Product) {
+	if product.price > 100 {
+		product.price += product.price * 0.2
+	}
+}
+func newProduct(name, category string, price float64) *Product {
+	return &Product{name, category, price}
 }
 
 func main() {
-	fmt.Println("Hello, Structs")
-
-	type Product struct {
-		name, category string
-		price          float64
-	}
-	type Item struct {
-		name     string
-		category string
-		price    float64
-	}
 
 	type StockLevel struct {
 		Product
 		Alternate Product
 		count     int
 	}
-	stockItem := StockLevel{
-		Product:   Product{"Kayak", "Watersports", 275.00},
-		Alternate: Product{"Lifejacket", "Watersports", 48.95},
-		count:     100,
+
+	array := [1]StockLevel{
+		{
+			Product:   Product{"Kayak1", "Watersports", 275.00},
+			Alternate: Product{"Lifejacket", "Watersports", 48.95},
+			count:     100,
+		},
 	}
-	fmt.Println("Name:", stockItem.Product.name)
-	fmt.Println("Count:", stockItem.count)
-	fmt.Println("Alt Name:", stockItem.Alternate.name, stockItem.name)
+	fmt.Println("Array:", array[0].Product.name)
+	slice := []StockLevel{
+		{
+			Product:   Product{"Kayak2", "Watersports", 275.00},
+			Alternate: Product{"Lifejacket", "Watersports", 48.95},
+			count:     100,
+		},
+	}
+	fmt.Println("Slice:", slice[0].Product.name)
+	kvp := map[string]StockLevel{
+		"kayak": {
+			Product:   Product{"Kayak3", "Watersports", 275.00},
+			Alternate: Product{"Lifejacket", "Watersports", 48.95},
+			count:     100,
+		},
+	}
+	fmt.Println("Map:", kvp["kayak"].Product.name)
+	fmt.Println(array)
+	fmt.Println(slice)
+	fmt.Println(kvp)
 
-	//change type and compare
-	prod := Product{name: "Kayak", category: "Watersports",
-		price: 275.00}
-	item := Item{name: "Kayak", category: "Watersports",
-		price: 275.00}
-	fmt.Println("prod == item:", prod == Product(item))
+	//copy and link
+	p1 := Product{
+		name:     "Kayak",
+		category: "Watersports",
+		price:    275,
+	}
+	p2 := p1
+	p3 := &p1
+	p1.name = "Original Kayak"
+	fmt.Println("P1:", p1.name)    // P1: Original Kayak
+	fmt.Println("P2:", p2.name)    //P2: Kayak
+	fmt.Println("P3:", (*p3).name) //!(*p3)!   P3: Original Kayak
 
-	//anonymous structure types
-	item2 := Item{name: "Stadium", category: "Soccer", price: 75000}
-	writeName(prod)
-	writeName(item2)
+	//struct pointer
+	kayak := Product{
+		name:     "Kayak",
+		category: "Watersports",
+		price:    275,
+	}
+	calcTax(&kayak)
+	fmt.Println("Name:", kayak.name, "Category:", kayak.category, "Price", kayak.price)
 
-	//anonymous structure: json
-	var builder strings.Builder
-	json.NewEncoder(&builder).Encode(struct {
-		ProductName  string
-		ProductPrice float64
-	}{
-		ProductName:  prod.name,
-		ProductPrice: prod.price,
-	})
-	fmt.Println(builder.String())
-
+	//function struct builder
+	products := [2]*Product{
+		newProduct("Kayak", "Watersports", 275),
+		newProduct("Hat", "Skiing", 42.50),
+	}
+	for _, p := range products {
+		fmt.Println("Name:", p.name, "Category:", p.category, "Price", p.price)
+		fmt.Println(*p)
+	}
 }
