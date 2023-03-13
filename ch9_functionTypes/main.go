cc2c9a0b75d83d619eb8f2675a1c88af0f5feb92
Oleg Vashkevich <ugluk@tut.by>
2023-03-13 12:00:23 +0300

    ch9: func as variable, argument and return value

--- ch9_functionTypes/main.go
+++ ch9_functionTypes/main.go
@@ -2,6 +2,57 @@ package main
 
 import "fmt"
 
+func calcWithTax(price float64) float64 {
+	return price + (price * 0.2)
+}
+func calcWithoutTax(price float64) float64 {
+	return price
+}
+
+func printPrice(product string, price float64, calculator func(float64) float64) {
+	fmt.Println("printPrice  -- ", "Product:", product, "Price:", calculator(price))
+}
+
+func selectCalculator(price float64) func(float64) float64 {
+	if price > 100 {
+		return calcWithTax
+	}
+	return calcWithoutTax
+}
+
 func main() {
 	fmt.Println("Hello, Function Types")
+
+	products := map[string]float64{
+		"Kayak":      275,
+		"Lifejacket": 48.95,
+	}
+
+	for product, price := range products {
+
+		//function as variable
+		var calcFunc func(float64) float64
+		fmt.Println("Function assigned:", calcFunc == nil)
+		if price > 100 {
+			calcFunc = calcWithTax
+		} else {
+			calcFunc = calcWithoutTax
+		}
+		fmt.Println("Function assigned:", calcFunc == nil)
+		totalPrice := calcFunc(price)
+		fmt.Println("Product:", product, "Price:",
+			totalPrice)
+
+		//function as argument
+		if price > 100 {
+			printPrice(product, price, calcWithTax)
+		} else {
+			printPrice(product, price, calcWithoutTax)
+		}
+
+		//function as return value
+		printPrice(product, price, selectCalculator(price))
+
+	}
+
 }
