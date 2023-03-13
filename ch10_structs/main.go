4fcd099992607fd77bf7c54f5319848c374ba51e
Oleg Vashkevich <ugluk@tut.by>
2023-03-13 13:34:01 +0300

    ch10: easy struct

--- ch10_structs/main.go
+++ ch10_structs/main.go
@@ -4,4 +4,25 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, Structs")
+
+	type Product struct {
+		name, category string
+		price          float64
+		int            //built-in field
+	}
+
+	kayak := Product{
+		name:     "Kayak",
+		category: "Watersports",
+		price:    275,
+	}
+	fmt.Println(kayak.name, kayak.category, kayak.price)
+	kayak.price = 300
+	fmt.Println("Changed price:", kayak.price)
+	//only positional arguments
+	var kayak2 = Product{"Kayak2", "Watersports2", 375.00, 1}
+	fmt.Println("Name:", kayak2.name)
+	fmt.Println("Category:", kayak2.category)
+	fmt.Println("Price:", kayak2.price)
+	fmt.Println(kayak)
 }
