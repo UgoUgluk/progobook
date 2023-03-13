011f47598102ce716a59fe651187df993403844a
Oleg Vashkevich <ugluk@tut.by>
2023-03-13 15:15:06 +0300

    ch10: finish

--- ch10_structs/main.go
+++ ch10_structs/main.go
@@ -6,83 +6,61 @@ import "fmt"
 type Product struct {
 	name, category string
 	price          float64
-	//otherNames []string
+	*Supplier
 }
 
-func calcTax(product *Product) {
-	if product.price > 100 {
-		product.price += product.price * 0.2
-	}
+// Supplier represents a supplier
+type Supplier struct {
+	name, city string
 }
-func newProduct(name, category string, price float64) *Product {
-	return &Product{name, category, price}
+
+func newProduct(
+	name, category string,
+	price float64,
+	supplier *Supplier,
+) *Product {
+	return &Product{name, category, price - 10, supplier}
+}
+func copyProduct(product *Product) Product {
+	p := *product
+	s := *product.Supplier
+	p.Supplier = &s
+	return p
 }
 
 func main() {
-
-	type StockLevel struct {
-		Product
-		Alternate Product
-		count     int
-	}
-
-	array := [1]StockLevel{
-		{
-			Product:   Product{"Kayak1", "Watersports", 275.00},
-			Alternate: Product{"Lifejacket", "Watersports", 48.95},
-			count:     100,
-		},
-	}
-	fmt.Println("Array:", array[0].Product.name)
-	slice := []StockLevel{
-		{
-			Product:   Product{"Kayak2", "Watersports", 275.00},
-			Alternate: Product{"Lifejacket", "Watersports", 48.95},
-			count:     100,
-		},
-	}
-	fmt.Println("Slice:", slice[0].Product.name)
-	kvp := map[string]StockLevel{
-		"kayak": {
-			Product:   Product{"Kayak3", "Watersports", 275.00},
-			Alternate: Product{"Lifejacket", "Watersports", 48.95},
-			count:     100,
-		},
-	}
-	fmt.Println("Map:", kvp["kayak"].Product.name)
-	fmt.Println(array)
-	fmt.Println(slice)
-	fmt.Println(kvp)
-
-	//copy and link
-	p1 := Product{
-		name:     "Kayak",
-		category: "Watersports",
-		price:    275,
-	}
-	p2 := p1
-	p3 := &p1
-	p1.name = "Original Kayak"
-	fmt.Println("P1:", p1.name)    // P1: Original Kayak
-	fmt.Println("P2:", p2.name)    //P2: Kayak
-	fmt.Println("P3:", (*p3).name) //!(*p3)!   P3: Original Kayak
-
-	//struct pointer
-	kayak := Product{
-		name:     "Kayak",
-		category: "Watersports",
-		price:    275,
-	}
-	calcTax(&kayak)
-	fmt.Println("Name:", kayak.name, "Category:", kayak.category, "Price", kayak.price)
-
-	//function struct builder
+	acme := &Supplier{"Acme Co", "New York"}
 	products := [2]*Product{
-		newProduct("Kayak", "Watersports", 275),
-		newProduct("Hat", "Skiing", 42.50),
+		newProduct("Kayak", "Watersports", 275, acme),
+		newProduct("Hat", "Skiing", 42.50, acme),
 	}
 	for _, p := range products {
-		fmt.Println("Name:", p.name, "Category:", p.category, "Price", p.price)
-		fmt.Println(*p)
+		fmt.Println("Name:", p.name, "Supplier:", p.Supplier.name, p.Supplier.city)
 	}
+
+	//copy
+	p1 := newProduct("Kayak", "Watersports", 275, acme)
+	p2 := *p1
+	p3 := copyProduct(p1)
+	p1.name = "Original Kayak"
+	p1.Supplier.name = "BoatCo"
+	for _, p := range []Product{*p1, p2} {
+		fmt.Println("Name:", p.name, "Supplier:", p.Supplier.name, p.Supplier.city)
+	}
+	//manual copy
+	for _, p := range []Product{*p1, p3} {
+		fmt.Println("Name:", p.name, "Supplier:", p.Supplier.name, p.Supplier.city)
+	}
+
+	//null error
+
+	//var prod Product
+	var prod Product = Product{Supplier: &Supplier{}}
+
+	var prodPtr *Product
+	fmt.Println("Value:", prod.name, prod.category,
+		prod.price,
+		prod.Supplier.name) //if var prod Product  - Supplier == nil, prod.Supplier.name - error
+	fmt.Println("Pointer:", prodPtr)
+
 }
