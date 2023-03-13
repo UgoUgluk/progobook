be15f429594e69f97c4e1d432b387e8f07e51aa5
Oleg Vashkevich <ugluk@tut.by>
2023-03-13 13:45:53 +0300

    ch10: anonymous structure types

--- ch10_structs/main.go
+++ ch10_structs/main.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+func writeName(val struct {
+	name, category string
+	price          float64
+}) {
+	fmt.Println("Name:", val.name)
+}
 
 func main() {
 	fmt.Println("Hello, Structs")
@@ -8,21 +19,48 @@ func main() {
 	type Product struct {
 		name, category string
 		price          float64
-		int            //built-in field
+	}
+	type Item struct {
+		name     string
+		category string
+		price    float64
 	}
 
-	kayak := Product{
-		name:     "Kayak",
-		category: "Watersports",
-		price:    275,
+	type StockLevel struct {
+		Product
+		Alternate Product
+		count     int
 	}
-	fmt.Println(kayak.name, kayak.category, kayak.price)
-	kayak.price = 300
-	fmt.Println("Changed price:", kayak.price)
-	//only positional arguments
-	var kayak2 = Product{"Kayak2", "Watersports2", 375.00, 1}
-	fmt.Println("Name:", kayak2.name)
-	fmt.Println("Category:", kayak2.category)
-	fmt.Println("Price:", kayak2.price)
-	fmt.Println(kayak)
+	stockItem := StockLevel{
+		Product:   Product{"Kayak", "Watersports", 275.00},
+		Alternate: Product{"Lifejacket", "Watersports", 48.95},
+		count:     100,
+	}
+	fmt.Println("Name:", stockItem.Product.name)
+	fmt.Println("Count:", stockItem.count)
+	fmt.Println("Alt Name:", stockItem.Alternate.name, stockItem.name)
+
+	//change type and compare
+	prod := Product{name: "Kayak", category: "Watersports",
+		price: 275.00}
+	item := Item{name: "Kayak", category: "Watersports",
+		price: 275.00}
+	fmt.Println("prod == item:", prod == Product(item))
+
+	//anonymous structure types
+	item2 := Item{name: "Stadium", category: "Soccer", price: 75000}
+	writeName(prod)
+	writeName(item2)
+
+	//anonymous structure: json
+	var builder strings.Builder
+	json.NewEncoder(&builder).Encode(struct {
+		ProductName  string
+		ProductPrice float64
+	}{
+		ProductName:  prod.name,
+		ProductPrice: prod.price,
+	})
+	fmt.Println(builder.String())
+
 }
