f1b8657f9070a4404314857e831e1916e84ea795
Oleg Vashkevich <ugluk@tut.by>
2023-03-14 09:42:02 +0300

    ch11: interface

--- ch11_methodAndInterfaces/main.go
+++ ch11_methodAndInterfaces/main.go
@@ -2,11 +2,41 @@ package main
 
 import "fmt"
 
+// Expense represents a expense
+type Expense interface {
+	getName() string
+	getCost(annual bool) float64
+}
+
+func calcTotal(expenses []Expense) (total float64) {
+	for _, item := range expenses {
+		total += item.getCost(true)
+	}
+	return
+}
+
+// Account represents a account
+type Account struct {
+	accountNumber int
+	expenses      []Expense
+}
+
 func main() {
-	kayak := Product{"Kayak", "Watersports", 275}
-	insurance := Service{"Boat Cover", 12, 89.50}
-	fmt.Println("Product:", kayak.name, "Price:",
-		kayak.price)
-	fmt.Println("Service:", insurance.description, "Price:",
-		insurance.monthlyFee*float64(insurance.durationMonths))
+	account := Account{
+		accountNumber: 12345,
+		expenses: []Expense{
+			Product{"Kayak", "Watersports", 275},
+			Service{"Boat Cover", 12, 89.50},
+		},
+	}
+
+	for _, expense := range account.expenses {
+		fmt.Println(
+			"Expense:", expense.getName(),
+			"Cost:", expense.getCost(true),
+		)
+	}
+
+	fmt.Println("Total:", calcTotal(account.expenses))
+
 }
