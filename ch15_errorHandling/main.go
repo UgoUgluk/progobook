46824595d45e71d067efa4267a14f81f9448af00
Oleg Vashkevich <ugluk@tut.by>
2023-03-14 18:53:55 +0300

    ch15: finish

--- ch15_errorHandling/main.go
+++ ch15_errorHandling/main.go
@@ -2,41 +2,49 @@ package main
 
 import "fmt"
 
-func main() {
+//CategoryCountMessage presente a CategoryCountMessage
+type CategoryCountMessage struct {
+	Category      string
+	Count         int
+	TerminalError interface{}
+}
+
+func processCategories(categories []string, outChan chan<- CategoryCountMessage) {
 	defer func() {
 		if arg := recover(); arg != nil {
-			if err, ok := arg.(error); ok {
-				fmt.Println("Error:", err.Error())
-				panic(err)
-			} else if str, ok := arg.(string); ok {
-				fmt.Println("Message:", str)
-			} else {
-				fmt.Println("Panic recovered")
+			fmt.Println(arg)
+			outChan <- CategoryCountMessage{
+				TerminalError: arg,
 			}
+			close(outChan)
 		}
 	}()
-
-	//categories := []string{"Watersports", "Chess"}
-	categories := []string{"Watersports", "Chess", "Running"}
-	for _, cat := range categories {
-		total, err := Products.TotalPrice(cat)
-		if err == nil {
-			fmt.Println(cat, "Total1:", ToCurrency(total))
-		} else {
-			fmt.Println(cat, "(no such category)")
-		}
-	}
-
-	//by goroutines and channels
 	channel := make(chan ChannelMessage, 10)
 	go Products.TotalPriceAsync(categories, channel)
 	for message := range channel {
 		if message.CategoryError == nil {
-			fmt.Println(message.Category, "Total2:", ToCurrency(message.Total))
+			outChan <- CategoryCountMessage{
+				Category: message.Category,
+				Count:    int(message.Total),
+			}
 		} else {
-			//fmt.Println(message.Category, "(no such category)")
 			panic(message.CategoryError)
 		}
+	}
+	close(outChan)
+}
+
+func main() {
+	categories := []string{"Watersports", "Chess", "Running"}
+	channel := make(chan CategoryCountMessage)
+	go processCategories(categories, channel)
+	for message := range channel {
+		if message.TerminalError == nil {
+			fmt.Println(message.Category, "Total:", message.Count)
+		} else {
+			fmt.Println("A terminal error occured")
+		}
+
 	}
 
 }
