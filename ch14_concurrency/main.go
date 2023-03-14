b661575a38775da457b837b08f9ed08aa7066d02
Oleg Vashkevich <ugluk@tut.by>
2023-03-14 17:52:50 +0300

    ch14: channels direction

--- ch14_concurrency/main.go
+++ ch14_concurrency/main.go
@@ -5,9 +5,24 @@ import (
 	//"time"
 )
 
+func receiveDispatches(channel <-chan DispatchNotification) {
+	for details := range channel {
+		fmt.Println(
+			"Dispatch to", details.Customer,
+			":", details.Quantity,
+			"x", details.Product.Name,
+		)
+	}
+	fmt.Println("Channel has been closed")
+}
+
 func main() {
-	fmt.Println("main function started")
-	CalcStoreTotal(Products)
-	//time.Sleep(time.Second * 5)
-	fmt.Println("main function complete")
+	dispatchChannel := make(chan DispatchNotification, 100)
+
+	//var sendOnlyChannel chan<- DispatchNotification = dispatchChannel
+	//var receiveOnlyChannel <-chan DispatchNotification = dispatchChannel
+
+	go DispatchOrders(chan<- DispatchNotification(dispatchChannel))
+	receiveDispatches((<-chan DispatchNotification)(dispatchChannel))
+
 }
---
+++ ch14_concurrency/orderdispatch.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// DispatchNotification presente a dispatch notification ))
+type DispatchNotification struct {
+	Customer string
+	*Product
+	Quantity int
+}
+
+// Customers presente a customers
+var Customers = []string{"Alice", "Bob", "Charlie", "Dora"}
+
+// DispatchOrders presente a DispatchOrders
+func DispatchOrders(channel chan<- DispatchNotification) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	orderCount := rand.Intn(3) + 2
+	fmt.Println("Order count:", orderCount)
+	for i := 0; i < orderCount; i++ {
+		channel <- DispatchNotification{
+			Customer: Customers[rand.Intn(len(Customers)-1)],
+			Quantity: rand.Intn(10),
+			Product:  ProductList[rand.Intn(len(ProductList)-1)],
+		}
+		//if i == 1 {
+		//	notification := <-channel
+		//	fmt.Println("Read:", notification.Customer)
+		//}
+
+	}
+	close(channel)
+}
