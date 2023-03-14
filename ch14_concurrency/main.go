9117268e259141184d11303a489856d6b59a1515
Oleg Vashkevich <ugluk@tut.by>
2023-03-14 18:11:59 +0300

    ch14: finish

--- ch14_concurrency/main.go
+++ ch14_concurrency/main.go
@@ -5,54 +5,29 @@ import (
 	"time"
 )
 
-func enumerateProducts(channel chan<- *Product) {
-	for _, p := range ProductList[:3] {
-		channel <- p
-		time.Sleep(time.Millisecond * 800)
+func enumerateProducts(channel1, channel2 chan<- *Product) {
+	for _, p := range ProductList {
+		select {
+		case channel1 <- p:
+			fmt.Println("Send via channel 1")
+		case channel2 <- p:
+			fmt.Println("Send via channel 2")
+		}
 	}
-	close(channel)
+	close(channel1)
+	close(channel2)
 }
 
 func main() {
-	dispatchChannel := make(chan DispatchNotification, 100)
-	go DispatchOrders(chan<- DispatchNotification(dispatchChannel))
-
-	productChannel := make(chan *Product)
-	go enumerateProducts(productChannel)
-	openChannels := 2
-
-	for {
-		select {
-		case details, ok := <-dispatchChannel:
-			if ok {
-				fmt.Println(
-					"Dispatch to", details.Customer,
-					":", details.Quantity,
-					"x", details.Product.Name,
-				)
-			} else {
-				fmt.Println("Dispatch channel has been closed")
-				dispatchChannel = nil
-				openChannels--
-			}
-		case product, ok := <-productChannel:
-			if ok {
-				fmt.Println("Product:", product.Name)
-			} else {
-				fmt.Println("Product channel has been closed")
-				productChannel = nil
-				openChannels--
-			}
-		default:
-			if openChannels == 0 {
-				goto alldone
-			}
-
-			fmt.Println("-- No message ready to be received")
-			time.Sleep(time.Millisecond * 500)
-		}
+	c1 := make(chan *Product, 2)
+	c2 := make(chan *Product, 2)
+	go enumerateProducts(c1, c2)
+	time.Sleep(time.Second)
+	for p := range c1 {
+		fmt.Println("Channel 1 received product:", p.Name)
 	}
-alldone:
-	fmt.Println("All values received")
+	for p := range c2 {
+		fmt.Println("Channel 2 received product:", p.Name)
+	}
 
 }
