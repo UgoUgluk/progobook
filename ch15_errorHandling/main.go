5420eb73fb95ade41aebc57c65b9db9a49f53d16
Oleg Vashkevich <ugluk@tut.by>
2023-03-14 18:34:09 +0300

    ch15: e.handling use channels

--- ch15_errorHandling/main.go
+++ ch15_errorHandling/main.go
@@ -3,9 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	categories := []string{"Watersports", "Chess"}
+	//categories := []string{"Watersports", "Chess"}
+	categories := []string{"Watersports", "Chess", "Running"}
 	for _, cat := range categories {
-		total := Products.TotalPrice(cat)
-		fmt.Println(cat, "Total:", ToCurrency(total))
+		total, err := Products.TotalPrice(cat)
+		if err == nil {
+			fmt.Println(cat, "Total1:", ToCurrency(total))
+		} else {
+			fmt.Println(cat, "(no such category)")
+		}
 	}
+
+	//by goroutines and channels
+	channel := make(chan ChannelMessage, 10)
+	go Products.TotalPriceAsync(categories, channel)
+	for message := range channel {
+		if message.CategoryError == nil {
+			fmt.Println(message.Category, "Total2:", ToCurrency(message.Total))
+		} else {
+			fmt.Println(message.Category, "(no such category)")
+		}
+	}
+
 }
--- ch15_errorHandling/operations.go
+++ ch15_errorHandling/operations.go
@@ -1,11 +1,45 @@
 package main
 
+//CategoryError error handling
+type CategoryError struct {
+	requestedCategory string
+}
+
+func (e *CategoryError) Error() string {
+	return "Category " + e.requestedCategory + " does not exist"
+}
+
+//ChannelMessage channel for errors
+type ChannelMessage struct {
+	Category string
+	Total    float64
+	*CategoryError
+}
+
 // TotalPrice calc sum prices of group
-func (slice ProductSlice) TotalPrice(category string) (total float64) {
+func (slice ProductSlice) TotalPrice(category string) (total float64, err *CategoryError) {
+	productCount := 0
 	for _, p := range slice {
 		if p.Category == category {
 			total += p.Price
+			productCount++
 		}
 	}
+	if productCount == 0 {
+		err = &CategoryError{requestedCategory: category}
+	}
 	return
+}
+
+//TotalPriceAsync method for send async data about total price
+func (slice ProductSlice) TotalPriceAsync(categories []string, channel chan<- ChannelMessage) {
+	for _, c := range categories {
+		total, err := slice.TotalPrice(c)
+		channel <- ChannelMessage{
+			Category:      c,
+			Total:         total,
+			CategoryError: err,
+		}
+	}
+	close(channel)
 }
