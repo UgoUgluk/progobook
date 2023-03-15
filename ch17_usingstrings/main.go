f40ff29eb26d3b139cc6489d07d8d807f1013755
Oleg Vashkevich <ugluk@tut.by>
2023-03-15 12:54:41 +0300

    ch17: finish

--- ch17_usingstrings/main.go
+++ ch17_usingstrings/main.go
@@ -20,49 +20,18 @@ func Printfln(template string, values ...interface{}) {
 }
 
 func main() {
-	fmt.Println("Product:", Kayak.Name, "Price:", Kayak.Price)
-	fmt.Print("Product:", Kayak.Name, "Price:", Kayak.Price, "\n")
-	fmt.Printf("Product: %v, Price: $%4.2f\n", Kayak.Name, Kayak.Price)
-
-	name, _ := getProductName(1)
-	fmt.Println(name)
-	_, err := getProductName(10)
-	fmt.Println(err.Error())
-
-	Printfln("Value: %v", Kayak)
-	Printfln("Fields+Value: %+v", Kayak)
-	Printfln("Go syntax: %#v", Kayak)
-	Printfln("Type: %T", Kayak)
-
-	//number
-	number := 250
-	Printfln("Binary: %b", number)
-	Printfln("Decimal: %d", number)
-	Printfln("Octal: %o, %O", number, number)
-	Printfln("Hexadecimal: %x, %X", number, number)
-
-	//float
-	float := 279.00
-	Printfln("Decimalless with exponent: %b", float)
-	Printfln("Decimal with exponent: %e", float)
-	Printfln("Decimal without exponent: %f", float)
-	Printfln("Hexadecimal: %x, %X", float, float)
-	Printfln("Decimal without exponent: >>%8.2f<<", float)
-	Printfln("Sign: >>%+.2f<<", float)
-	Printfln("Zeros for Padding: >>%010.2f<<", float)
-	Printfln("Right Padding: >>%-8.2f<<", float)
-
-	//string
-	name2 := "Kayak"
-	Printfln("String: %s", name2)
-	Printfln("Character: %c", []rune(name2)[0])
-	Printfln("Unicode: %U", []rune(name2)[0])
-
-	//bool
-	Printfln("Bool: %t", len(name2) > 1)
-	Printfln("Bool: %t", len(name2) > 100)
-
-	//Pointer
-	Printfln("Pointer: %p", &name2)
-
+	var name string
+	var category string
+	var price float64
+	fmt.Print("Enter text to scan: ")
+	n, err := fmt.Scanln(&name, &category, &price)
+	if err == nil {
+		Printfln("Scanned %v values", n)
+		Printfln(
+			"Name: %v, Category: %v, Price: %.2f",
+			name, category, price,
+		)
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
 }
