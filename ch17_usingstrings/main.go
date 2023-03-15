a36a3da7e2b0d5cc483f2ccc4efbe426514d1d8d
Oleg Vashkevich <ugluk@tut.by>
2023-03-15 12:45:08 +0300

    ch17: formatting

--- ch17_usingstrings/main.go
+++ ch17_usingstrings/main.go
@@ -2,6 +2,67 @@ package main
 
 import "fmt"
 
+func getProductName(index int) (name string, err error) {
+	if len(Products) > index {
+		name = fmt.Sprintf(
+			"getProductName: %v",
+			Products[index].Name,
+		)
+	} else {
+		err = fmt.Errorf("Error for index %v", index)
+	}
+	return
+}
+
+//Printfln printf line
+func Printfln(template string, values ...interface{}) {
+	fmt.Printf(template+"\n", values...)
+}
+
 func main() {
 	fmt.Println("Product:", Kayak.Name, "Price:", Kayak.Price)
+	fmt.Print("Product:", Kayak.Name, "Price:", Kayak.Price, "\n")
+	fmt.Printf("Product: %v, Price: $%4.2f\n", Kayak.Name, Kayak.Price)
+
+	name, _ := getProductName(1)
+	fmt.Println(name)
+	_, err := getProductName(10)
+	fmt.Println(err.Error())
+
+	Printfln("Value: %v", Kayak)
+	Printfln("Fields+Value: %+v", Kayak)
+	Printfln("Go syntax: %#v", Kayak)
+	Printfln("Type: %T", Kayak)
+
+	//number
+	number := 250
+	Printfln("Binary: %b", number)
+	Printfln("Decimal: %d", number)
+	Printfln("Octal: %o, %O", number, number)
+	Printfln("Hexadecimal: %x, %X", number, number)
+
+	//float
+	float := 279.00
+	Printfln("Decimalless with exponent: %b", float)
+	Printfln("Decimal with exponent: %e", float)
+	Printfln("Decimal without exponent: %f", float)
+	Printfln("Hexadecimal: %x, %X", float, float)
+	Printfln("Decimal without exponent: >>%8.2f<<", float)
+	Printfln("Sign: >>%+.2f<<", float)
+	Printfln("Zeros for Padding: >>%010.2f<<", float)
+	Printfln("Right Padding: >>%-8.2f<<", float)
+
+	//string
+	name2 := "Kayak"
+	Printfln("String: %s", name2)
+	Printfln("Character: %c", []rune(name2)[0])
+	Printfln("Unicode: %U", []rune(name2)[0])
+
+	//bool
+	Printfln("Bool: %t", len(name2) > 1)
+	Printfln("Bool: %t", len(name2) > 100)
+
+	//Pointer
+	Printfln("Pointer: %p", &name2)
+
 }
--- ch17_usingstrings/product.go
+++ ch17_usingstrings/product.go
@@ -6,6 +6,16 @@ type Product struct {
 	Price          float64
 }
 
+//change %v and %+v
+/*func (p Product) String() string {
+	return fmt.Sprintf("Product: %v, Price: $%4.2f", p.Name, p.Price)
+}*/
+
+//change %#v
+/*func (p Product) GoString() string {
+	return fmt.Sprintf("Product: %v, Price: $%4.2f", p.Name, p.Price)
+}*/
+
 //Kayak presente a product Kayak
 var Kayak = Product{
 	Name:     "Kayak",
