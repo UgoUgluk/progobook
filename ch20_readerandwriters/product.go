56acb2c01cb095199e2c6a28f6f49b44993ac4d8
Oleg Vashkevich <ugluk@tut.by>
2023-03-16 15:17:03 +0300

    ch20: start

---
+++ ch20_readerandwriters/product.go
@@ -0,0 +1,26 @@
+package main
+
+//Product presente a product
+type Product struct {
+	Name, Category string
+	Price          float64
+}
+
+//Kayak presente a product Kayak
+var Kayak = Product{
+	Name:     "Kayak",
+	Category: "Watersports",
+	Price:    279,
+}
+
+//Products presente a products
+var Products = []Product{
+	{"Kayak", "Watersports", 279},
+	{"Lifejacket", "Watersports", 49.95},
+	{"Soccer Ball", "Soccer", 19.50},
+	{"Corner Flags", "Soccer", 34.95},
+	{"Stadium", "Soccer", 79500},
+	{"Thinking Cap", "Chess", 16},
+	{"Unsteady Chair", "Chess", 75},
+	{"Bling-Bling King", "Chess", 1200},
+}
