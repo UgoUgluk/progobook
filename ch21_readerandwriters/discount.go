8c5ed728333a83a1036166571b72861e9957395a
Oleg Vashkevich <ugluk@tut.by>
2023-03-17 17:54:48 +0300

    c21: finish decode

--- ch21_readerandwriters/discount.go
+++ ch21_readerandwriters/discount.go
@@ -1,17 +1,21 @@
 package main
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"strconv"
+)
 
-//DiscountedProduct Product with Discount
+// DiscountedProduct Product with Discount
 type DiscountedProduct struct {
 	//*Product
 	//*Product `json:"product,omitempty"`
 	*Product `json:",omitempty"`
 	//Discount float64 `json:"-"`
-	Discount float64 `json:",string"`
+	//Discount float64 `json:",string"`
+	Discount float64 `json:"offer,string"`
 }
 
-//MarshalJSON other json out
+// MarshalJSON other json out
 func (dp *DiscountedProduct) MarshalJSON() (jsn []byte, err error) {
 	if dp.Product != nil {
 		m := map[string]interface{}{
@@ -19,6 +23,33 @@ func (dp *DiscountedProduct) MarshalJSON() (jsn []byte, err error) {
 			"cost":    dp.Price - dp.Discount,
 		}
 		jsn, err = json.Marshal(m)
+	}
+	return
+}
+
+// UnmarshalJSON other json decode
+func (dp *DiscountedProduct) UnmarshalJSON(data []byte) (err error) {
+	mdata := map[string]interface{}{}
+	err = json.Unmarshal(data, &mdata)
+	if dp.Product == nil {
+		dp.Product = &Product{}
+	}
+	if err == nil {
+		if name, ok := mdata["Name"].(string); ok {
+			dp.Name = name
+		}
+		if category, ok := mdata["Category"].(string); ok {
+			dp.Category = category
+		}
+		if price, ok := mdata["Price"].(float64); ok {
+			dp.Price = price
+		}
+		if discount, ok := mdata["Offer"].(string); ok {
+			fpval, fperr := strconv.ParseFloat(discount, 64)
+			if fperr == nil {
+				dp.Discount = fpval
+			}
+		}
 	}
 	return
 }
--- ch21_readerandwriters/main.go
+++ ch21_readerandwriters/main.go
@@ -2,46 +2,137 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"io"
 	"strings"
 )
 
 func main() {
-	names := []string{"Kayak", "Lifejacket", "Soccer Ball"}
-	numbers := [3]int{10, 20, 30}
-	var byteArray [5]byte
-	copy(byteArray[0:], []byte(names[0]))
-	byteSlice := []byte(names[0])
+	reader := strings.NewReader(`true "Hello" 99.99 200`)
+	vals := []interface{}{}
+	decoder := json.NewDecoder(reader)
+	decoder.UseNumber()
 
-	m := map[string]float64{
-		"Kayak":      279,
-		"Lifejacket": 49.95,
+	for {
+		var decodedVal interface{}
+		err := decoder.Decode(&decodedVal)
+		if err != nil {
+			if err != io.EOF {
+				Printfln("Error: %v", err.Error())
+			}
+			break
+		}
+		vals = append(vals, decodedVal)
+	}
+	for _, val := range vals {
+		Printfln("Decoded (%T): %v", val, val)
 	}
 
-	dp := DiscountedProduct{
-		Product:  &Kayak,
-		Discount: 10.50,
+	//num
+	for _, val := range vals {
+		if num, ok := val.(json.Number); ok {
+			if ival, err := num.Int64(); err == nil {
+				Printfln("Decoded Integer: %v", ival)
+			} else if fpval, err := num.Float64(); err == nil {
+				Printfln("Decoded Floating Point: %v", fpval)
+			} else {
+				Printfln("Decoded String: %v", num.String())
+			}
+		} else {
+			Printfln("Decoded (%T): %v", val, val)
+		}
 	}
 
-	var writer strings.Builder
-	encoder := json.NewEncoder(&writer)
-	encoder.Encode(names)
-	encoder.Encode(numbers)
-	encoder.Encode(byteArray)
-	encoder.Encode(byteSlice)
+	//with types
+	reader2 := strings.NewReader(`true "Hello" 99.99 200`)
+	var bval bool
+	var sval string
+	var fpval float64
+	var ival int
+	vals2 := []interface{}{&bval, &sval, &fpval, &ival}
+	decoder2 := json.NewDecoder(reader2)
+	for i := 0; i < len(vals2); i++ {
+		err := decoder2.Decode(vals2[i])
+		if err != nil {
+			Printfln("Error: %v", err.Error())
+			break
+		}
+	}
+	Printfln("Decoded (%T): %v", bval, bval)
+	Printfln("Decoded (%T): %v", sval, sval)
+	Printfln("Decoded (%T): %v", fpval, fpval)
+	Printfln("Decoded (%T): %v", ival, ival)
 
-	encoder.Encode(m)
+	//arrays
+	reader3 := strings.NewReader(`[10,20,30]["Kayak","Lifejacket",279]`)
+	vals3 := []interface{}{}
+	decoder3 := json.NewDecoder(reader3)
+	for {
+		var decodedVal interface{}
+		err := decoder3.Decode(&decodedVal)
+		if err != nil {
+			if err != io.EOF {
+				Printfln("Error: %v", err.Error())
+			}
+			break
+		}
+		vals3 = append(vals3, decodedVal)
+	}
+	for _, val := range vals3 {
+		Printfln("Decoded (%T): %v", val, val)
+	}
 
-	encoder.Encode(Kayak)
+	//maps
+	reader4 := strings.NewReader(`{"Kayak" : 279, "Lifejacket" : 49.95}`)
+	//m := map[string]interface{}{}
+	m := map[string]float64{}
+	decoder4 := json.NewDecoder(reader4)
+	err := decoder4.Decode(&m)
+	if err != nil {
+		Printfln("Error: %v", err.Error())
+	} else {
+		Printfln("Map: %T, %v", m, m)
+		for k, v := range m {
+			Printfln("Key: %v, Value: %v", k, v)
+		}
+	}
 
-	encoder.Encode(&dp)
+	//struct
+	reader5 := strings.NewReader(`
+		{"Name":"Kayak","Category":"Watersports","Price":279}
+		{"Name":"Lifejacket","Category":"Watersports" }
+		{"name":"Canoe","category":"Watersports", "price": 100, "inStock": true }
+	`)
+	decoder5 := json.NewDecoder(reader5)
+	decoder5.DisallowUnknownFields()
+	for {
+		var val Product
+		err := decoder5.Decode(&val)
+		if err != nil {
+			if err != io.EOF {
+				Printfln("Error: %v", err.Error())
+			}
+			break
+		} else {
+			Printfln("Name: %v, Category: %v, Price: %v",
+				val.Name, val.Category, val.Price)
+		}
+	}
 
-	dp2 := DiscountedProduct{Discount: 10.50}
-	encoder.Encode(&dp2)
-
-	namedItems := []Named{&dp, &Person{PersonName: "Alice"}}
-	encoder.Encode(namedItems)
-
-	fmt.Print(writer.String())
+	//struct with tags
+	reader6 := strings.NewReader(`{"Name":"Kayak","Category":"Watersports","Price":279, "Offer":"10"}`)
+	decoder6 := json.NewDecoder(reader6)
+	for {
+		var val DiscountedProduct
+		err := decoder6.Decode(&val)
+		if err != nil {
+			if err != io.EOF {
+				Printfln("Error: %v", err.Error())
+			}
+			break
+		} else {
+			Printfln("Name: %v, Category: %v, Price: %v, Discount: %v",
+				val.Name, val.Category, val.Price, val.Discount)
+		}
+	}
 
 }
