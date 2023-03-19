f9ab9954143c73113cce00b436812efaa4d8e581
Oleg Vashkevich <ugluk@tut.by>
2023-03-19 13:27:28 +0300

    ch23: template execute

--- ch23_htmltext/main.go
+++ ch23_htmltext/main.go
@@ -1,8 +1,44 @@
 package main
 
+import (
+	"html/template"
+	"os"
+)
+
+// Exec Exec Template
+func Exec(t *template.Template) error {
+	return t.Execute(os.Stdout, &Kayak)
+}
+
 func main() {
-	for _, p := range Products {
-		Printfln("Product: %v, Category: %v, Price: $%.2f",
-			p.Name, p.Category, p.Price)
+	//ParseFiles
+	allTemplates, err1 := template.ParseFiles("templates/template.html", "templates/extras.html")
+	if err1 == nil {
+		allTemplates.ExecuteTemplate(os.Stdout, "template.html", &Kayak)
+		os.Stdout.WriteString("\n")
+		allTemplates.ExecuteTemplate(os.Stdout, "extras.html", &Kayak)
+	} else {
+		Printfln("Error: %v %v", err1.Error())
+	}
+
+	//ParseGlob
+	os.Stdout.WriteString("\n")
+	allTemplates, err := template.ParseGlob("templates/*.html")
+	if err == nil {
+		for _, t := range allTemplates.Templates() {
+			Printfln("Template name: %v", t.Name())
+		}
+	} else {
+		Printfln("Error: %v %v", err.Error())
+	}
+
+	//Lookup
+	allTemplates, err2 := template.ParseGlob("templates/*.html")
+	if err2 == nil {
+		selectedTemplated := allTemplates.Lookup("template.html")
+		err = Exec(selectedTemplated)
+	}
+	if err != nil {
+		Printfln("Error: %v %v", err.Error())
 	}
 }
