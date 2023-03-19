2b98fc53924a817e6a204ffaf0ad19b370954b2c
Oleg Vashkevich <ugluk@tut.by>
2023-03-19 13:43:33 +0300

    ch23: define

--- ch23_htmltext/main.go
+++ ch23_htmltext/main.go
@@ -12,34 +12,11 @@ func Exec(t *template.Template) error {
 }
 
 func main() {
-	//ParseFiles
-	allTemplates, err1 := template.ParseFiles("templates/template.html", "templates/extras.html")
-	if err1 == nil {
-		allTemplates.ExecuteTemplate(os.Stdout, "template.html", &Kayak)
-		os.Stdout.WriteString("\n")
-		allTemplates.ExecuteTemplate(os.Stdout, "extras.html", &Kayak)
-	} else {
-		Printfln("Error: %v %v", err1.Error())
-	}
-
-	//ParseGlob
-	os.Stdout.WriteString("\n")
+	//Lookup
 	allTemplates, err := template.ParseGlob("templates/*.html")
 	if err == nil {
-		for _, t := range allTemplates.Templates() {
-			Printfln("Template name: %v", t.Name())
-		}
-	} else {
-		Printfln("Error: %v %v", err.Error())
-	}
-
-	//Lookup
-	allTemplates, err2 := template.ParseGlob("templates/*.html")
-	if err2 == nil {
-		selectedTemplated := allTemplates.Lookup("template.html")
+		//selectedTemplated := allTemplates.Lookup("template.html")
+		selectedTemplated := allTemplates.Lookup("mainTemplate")
 		err = Exec(selectedTemplated)
-	}
-	if err != nil {
-		Printfln("Error: %v %v", err.Error())
 	}
 }
