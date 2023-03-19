a2cd767ad77d31c2957dc9a366776d89f7c3ce11
Oleg Vashkevich <ugluk@tut.by>
2023-03-19 10:19:03 +0300

    ch22: filepath and MkdirAll

--- ch22_files/main.go
+++ ch22_files/main.go
@@ -1,51 +1,36 @@
 package main
 
 import (
+	// "fmt"
+	// "time"
 	"encoding/json"
-	"fmt"
 	"os"
-	"time"
+	"path/filepath"
 )
 
 func main() {
-	total := 0.0
-	for _, p := range Products {
-		total += p.Price
-	}
-	dataStr := fmt.Sprintf("Time: %v, Total: $%.2f\n", time.Now().Format("Mon 15:04:05"), total)
-
-	//WriteFile
-	err := os.WriteFile("output.txt", []byte(dataStr), 0666)
+	path, err := os.UserHomeDir()
 	if err == nil {
-		fmt.Println("Output file created")
-	} else {
-		Printfln("Error: %v", err.Error())
+		path = filepath.Join(path, "MyApp", "MyTempFile.json")
 	}
+	Printfln("Full path: %v", path)
+	Printfln("Volume name: %v", filepath.VolumeName(path))
+	Printfln("Dir component: %v", filepath.Dir(path))
+	Printfln("File component: %v", filepath.Base(path))
+	Printfln("File extension: %v", filepath.Ext(path))
 
-	//OpenFile
-	file, err := os.OpenFile("output2.txt",
-		os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	err = os.MkdirAll(filepath.Dir(path), 0766)
 	if err == nil {
-		defer file.Close()
-		file.WriteString(dataStr)
-	} else {
-		Printfln("Error: %v", err.Error())
-	}
-
-	//json
-	cheapProducts := []Product{}
-	for _, p := range Products {
-		if p.Price < 100 {
-			cheapProducts = append(cheapProducts, p)
+		file, err := os.OpenFile(path, os.O_CREATE|
+			os.O_WRONLY, 0666)
+		if err == nil {
+			defer file.Close()
+			encoder := json.NewEncoder(file)
+			encoder.Encode(Products)
 		}
 	}
-	file2, err := os.OpenFile("cheap.json", os.O_WRONLY|os.O_CREATE, 0666)
-	if err == nil {
-		defer file2.Close()
-		encoder := json.NewEncoder(file2)
-		encoder.Encode(cheapProducts)
-	} else {
-		Printfln("Error: %v", err.Error())
+	if err != nil {
+		Printfln("Error %v", err.Error())
 	}
 
 }
