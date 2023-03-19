2d4c96533d5540ce593a58bba6d209fe28b2b986
Oleg Vashkevich <ugluk@tut.by>
2023-03-19 10:25:09 +0300

    ch22: finish

--- ch22_files/main.go
+++ ch22_files/main.go
@@ -1,35 +1,65 @@
 package main
 
 import (
-	// "fmt"
-	// "time"
-	"encoding/json"
 	"os"
 	"path/filepath"
 )
 
-func main() {
-	path, err := os.UserHomeDir()
-	if err == nil {
-		path = filepath.Join(path, "MyApp", "MyTempFile.json")
-	}
-	Printfln("Full path: %v", path)
-	Printfln("Volume name: %v", filepath.VolumeName(path))
-	Printfln("Dir component: %v", filepath.Dir(path))
-	Printfln("File component: %v", filepath.Base(path))
-	Printfln("File extension: %v", filepath.Ext(path))
+func callback(path string, dir os.DirEntry, dirErr error) (err error) {
+	info, _ := dir.Info()
+	Printfln("Path %v, Size: %v", path, info.Size())
+	return
+}
 
-	err = os.MkdirAll(filepath.Dir(path), 0766)
+func main() {
+	//ReadDir
+	path, err := os.Getwd()
 	if err == nil {
-		file, err := os.OpenFile(path, os.O_CREATE|
-			os.O_WRONLY, 0666)
+		dirEntries, err := os.ReadDir(path)
 		if err == nil {
-			defer file.Close()
-			encoder := json.NewEncoder(file)
-			encoder.Encode(Products)
+			for _, dentry := range dirEntries {
+				Printfln("Entry name: %v, IsDir: %v",
+					dentry.Name(), dentry.IsDir())
+			}
 		}
 	}
 	if err != nil {
+		Printfln("Error %v", err.Error())
+	}
+
+	//Stat
+	targetFiles := []string{"no_such_file.txt", "config.json"}
+	for _, name := range targetFiles {
+		info, err := os.Stat(name)
+		if os.IsNotExist(err) {
+			Printfln("File does not exist: %v", name)
+		} else if err != nil {
+			Printfln("Other error: %v", err.Error())
+		} else {
+			Printfln("File %v, Size: %v", info.Name(),
+				info.Size())
+		}
+	}
+
+	//Glob
+	path2, err := os.Getwd()
+	if err == nil {
+		matches, err := filepath.Glob(filepath.Join(path2, "*.json"))
+		if err == nil {
+			for _, m := range matches {
+				Printfln("Match: %v", m)
+			}
+		}
+	}
+	if err != nil {
+		Printfln("Error %v", err.Error())
+	}
+
+	//WalkDir
+	path3, err := os.Getwd()
+	if err == nil {
+		err = filepath.WalkDir(path3, callback)
+	} else {
 		Printfln("Error %v", err.Error())
 	}
 
