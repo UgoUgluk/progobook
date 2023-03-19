ac6bc4bff490964618e8c3cd01934d30a25ca2c0
Oleg Vashkevich <ugluk@tut.by>
2023-03-19 13:35:02 +0300

    ch23: range, if, slice

--- ch23_htmltext/main.go
+++ ch23_htmltext/main.go
@@ -7,7 +7,8 @@ import (
 
 // Exec Exec Template
 func Exec(t *template.Template) error {
-	return t.Execute(os.Stdout, &Kayak)
+	//return t.Execute(os.Stdout, &Kayak)
+	return t.Execute(os.Stdout, Products)
 }
 
 func main() {
