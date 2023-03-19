16107bbebe753e73ac94f8b8cef95a29c90f6484
Oleg Vashkevich <ugluk@tut.by>
2023-03-19 13:06:42 +0300

    ch23: start

---
+++ ch23_htmltext/product.go
@@ -0,0 +1,36 @@
+package main
+
+//Product presente a product
+type Product struct {
+	Name, Category string
+	Price          float64
+}
+
+//Kayak presente a Kayak
+var Kayak = Product{
+	Name:     "Kayak",
+	Category: "Watersports",
+	Price:    279,
+}
+
+//Products presente a products
+var Products = []Product{
+	{"Kayak", "Watersports", 279},
+	{"Lifejacket", "Watersports", 49.95},
+	{"Soccer Ball", "Soccer", 19.50},
+	{"Corner Flags", "Soccer", 34.95},
+	{"Stadium", "Soccer", 79500},
+	{"Thinking Cap", "Chess", 16},
+	{"Unsteady Chair", "Chess", 75},
+	{"Bling-Bling King", "Chess", 1200},
+}
+
+//AddTax calc tax
+func (p *Product) AddTax() float64 {
+	return p.Price * 1.2
+}
+
+//ApplyDiscount calc discount
+func (p *Product) ApplyDiscount(amount float64) float64 {
+	return p.Price - amount
+}
