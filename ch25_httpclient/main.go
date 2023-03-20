6c8dca49956cfc9a2b002e758e74df3dee4e8f99
Oleg Vashkevich <ugluk@tut.by>
2023-03-20 17:44:44 +0300

    ch25: DefaultClient, CookieJar, Redirect

--- ch25_httpclient/main.go
+++ ch25_httpclient/main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
-	"strings"
 	"time"
 )
 
@@ -13,33 +12,49 @@ func main() {
 	go http.ListenAndServe(":5000", nil)
 	time.Sleep(time.Second)
 
-	/*formData := map[string][]string{
-		"name":     {"Kayak "},
-		"category": {"Watersports"},
-		"price":    {"279"},
-	}
+	/*clients := make([]http.Client, 3)
+	for index, client := range clients {
 
-	response, err := http.PostForm("http://localhost:5000/echo", formData)
-	if err == nil && response.StatusCode == http.StatusOK {
-		io.Copy(os.Stdout, response.Body)
-		defer response.Body.Close()
-	} else {
-		Printfln("Error: %v", err.Error())
+		jar, err := cookiejar.New(nil)
+		if err == nil {
+			client.Jar = jar
+		}
+		for i := 0; i < 3; i++ {
+			req, err := http.NewRequest(http.MethodGet,
+				"http://localhost:5000/cookie", nil)
+			if err == nil {
+				response, err := client.Do(req)
+				if err == nil && response.StatusCode == http.StatusOK {
+					fmt.Fprintf(os.Stdout, "Client %v: ", index)
+					io.Copy(os.Stdout, response.Body)
+					defer response.Body.Close()
+				} else {
+					Printfln("Request Error: %v", err.Error())
+				}
+			} else {
+				Printfln("Request Init Error: %v", err.Error())
+			}
+		}
 	}*/
 
-	var builder strings.Builder
-	err := json.NewEncoder(&builder).Encode(Products[0])
+	//redirects
+	http.DefaultClient.CheckRedirect = func(req *http.Request,
+		previous []*http.Request) error {
+		if len(previous) == 3 {
+			url, _ := url.Parse("http://localhost:5000/html")
+			req.URL = url
+		}
+		return nil
+	}
+
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:5000/redirect1", nil)
 	if err == nil {
-		response, err := http.Post(
-			"http://localhost:5000/echo",
-			"application/json",
-			strings.NewReader(builder.String()),
-		)
-		if err == nil && response.StatusCode == http.StatusOK {
+		var response *http.Response
+		response, err = http.DefaultClient.Do(req)
+		if err == nil {
 			io.Copy(os.Stdout, response.Body)
-			defer response.Body.Close()
 		} else {
-			Printfln("Error: %v", err.Error())
+			Printfln("Request Error: %v", err.Error())
 		}
 	} else {
 		Printfln("Error: %v", err.Error())
