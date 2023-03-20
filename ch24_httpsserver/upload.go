6cc6a067edd6dbc795be6e4f6e5d071589b9596c
Oleg Vashkevich <ugluk@tut.by>
2023-03-20 16:57:43 +0300

    ch24: finish

--- ch24_httpsserver/upload.go
+++ ch24_httpsserver/upload.go
@@ -8,22 +8,24 @@ import (
 
 // HandleMultipartForm handle multipart form
 func HandleMultipartForm(writer http.ResponseWriter, request *http.Request) {
+	request.ParseMultipartForm(10000000)
 	fmt.Fprintf(writer, "Name: %v, City: %v\n",
-		request.FormValue("name"),
-		request.FormValue("city"))
+		request.MultipartForm.Value["name"][0],
+		request.MultipartForm.Value["city"][0])
 	fmt.Fprintln(writer, "------")
-	file, header, err := request.FormFile("files")
-	if err == nil {
-		defer file.Close()
+	for _, header := range request.MultipartForm.File["files"] {
 		fmt.Fprintf(writer, "Name: %v, Size: %v\n", header.Filename,
 			header.Size)
-		for k, v := range header.Header {
-			fmt.Fprintf(writer, "Key: %v, Value: %v\n", k, v)
+		file, err := header.Open()
+		if err == nil {
+			defer file.Close()
+			fmt.Fprintln(writer, "------")
+			io.Copy(writer, file)
+		} else {
+			http.Error(writer, err.Error(),
+				http.StatusInternalServerError)
+			return
 		}
-		fmt.Fprintln(writer, "------")
-		io.Copy(writer, file)
-	} else {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
 	}
 }
 func init() {
