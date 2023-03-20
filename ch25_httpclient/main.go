7c4ff59ee4f097281884399b42c6ee769218db93
Oleg Vashkevich <ugluk@tut.by>
2023-03-20 17:47:43 +0300

    ch25: finish

--- ch25_httpclient/main.go
+++ ch25_httpclient/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"io"
+	"mime/multipart"
 	"net/http"
-	"net/url"
 	"os"
 	"time"
 )
@@ -12,42 +13,30 @@ func main() {
 	go http.ListenAndServe(":5000", nil)
 	time.Sleep(time.Second)
 
-	/*clients := make([]http.Client, 3)
-	for index, client := range clients {
-
-		jar, err := cookiejar.New(nil)
-		if err == nil {
-			client.Jar = jar
-		}
-		for i := 0; i < 3; i++ {
-			req, err := http.NewRequest(http.MethodGet,
-				"http://localhost:5000/cookie", nil)
-			if err == nil {
-				response, err := client.Do(req)
-				if err == nil && response.StatusCode == http.StatusOK {
-					fmt.Fprintf(os.Stdout, "Client %v: ", index)
-					io.Copy(os.Stdout, response.Body)
-					defer response.Body.Close()
-				} else {
-					Printfln("Request Error: %v", err.Error())
-				}
-			} else {
-				Printfln("Request Init Error: %v", err.Error())
-			}
-		}
-	}*/
-
-	//redirects
-	http.DefaultClient.CheckRedirect = func(req *http.Request,
-		previous []*http.Request) error {
-		if len(previous) == 3 {
-			url, _ := url.Parse("http://localhost:5000/html")
-			req.URL = url
-		}
-		return nil
+	var buffer bytes.Buffer
+	formWriter := multipart.NewWriter(&buffer)
+	fieldWriter, err := formWriter.CreateFormField("name")
+	if err == nil {
+		io.WriteString(fieldWriter, "Alice")
 	}
+	fieldWriter, err = formWriter.CreateFormField("city")
+	if err == nil {
+		io.WriteString(fieldWriter, "New York")
+	}
+	fileWriter, err := formWriter.CreateFormFile("codeFile",
+		"printer.go")
+	if err == nil {
+		fileData, err := os.ReadFile("./printer.go")
+		if err == nil {
+			fileWriter.Write(fileData)
+		}
+	}
+	formWriter.Close()
+	req, err := http.NewRequest(http.MethodPost,
+		"http://localhost:5000/form", &buffer)
+	req.Header["Content-Type"] = []string{
+		formWriter.FormDataContentType()}
 
-	req, err := http.NewRequest(http.MethodGet, "http://localhost:5000/redirect1", nil)
 	if err == nil {
 		var response *http.Response
 		response, err = http.DefaultClient.Do(req)
