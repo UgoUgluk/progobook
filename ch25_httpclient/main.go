87ce6c53e384b2ab1ea646ad6dab4c7870ef0ec0
Oleg Vashkevich <ugluk@tut.by>
2023-03-20 17:24:45 +0300

    ch25: Get, Post, PostfForm

--- ch25_httpclient/main.go
+++ ch25_httpclient/main.go
@@ -1,10 +1,48 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-	Printfln("Starting HTTP Server")
-	http.ListenAndServe(":5000", nil)
+	go http.ListenAndServe(":5000", nil)
+	time.Sleep(time.Second)
+
+	/*formData := map[string][]string{
+		"name":     {"Kayak "},
+		"category": {"Watersports"},
+		"price":    {"279"},
+	}
+
+	response, err := http.PostForm("http://localhost:5000/echo", formData)
+	if err == nil && response.StatusCode == http.StatusOK {
+		io.Copy(os.Stdout, response.Body)
+		defer response.Body.Close()
+	} else {
+		Printfln("Error: %v", err.Error())
+	}*/
+
+	var builder strings.Builder
+	err := json.NewEncoder(&builder).Encode(Products[0])
+	if err == nil {
+		response, err := http.Post(
+			"http://localhost:5000/echo",
+			"application/json",
+			strings.NewReader(builder.String()),
+		)
+		if err == nil && response.StatusCode == http.StatusOK {
+			io.Copy(os.Stdout, response.Body)
+			defer response.Body.Close()
+		} else {
+			Printfln("Error: %v", err.Error())
+		}
+	} else {
+		Printfln("Error: %v", err.Error())
+	}
+
 }
