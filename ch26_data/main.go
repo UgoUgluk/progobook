01dbb2dc47751afa4889c0db57ce44eaf628a477
Oleg Vashkevich <ugluk@tut.by>
2023-03-23 09:58:32 +0300

    ch26: Query + Scan

--- ch26_data/main.go
+++ ch26_data/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"database/sql"
+)
+
+func queryDatabase(db *sql.DB) {
+	rows, err := db.Query("SELECT * from Products")
+	if err == nil {
+		for rows.Next() {
+			var id, category int
+			var name int
+			var price float64
+			scanErr := rows.Scan(&id, &name, &category, &price)
+			if scanErr == nil {
+				Printfln("Row: %v %v %v %v", id, name, category,
+					price)
+			} else {
+				Printfln("Scan error: %v", scanErr)
+				break
+			}
+		}
+	} else {
+		Printfln("Error: %v", err)
+	}
+}
+
 func main() {
-	Printfln("Hello, Data")
+	//listDrivers()
+	db, err := openDatabase()
+	if err == nil {
+		queryDatabase(db)
+		db.Close()
+	} else {
+		panic(err)
+	}
 }
