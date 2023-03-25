bfa07cfef756d80fd087b231f1c5d4311675168d
Oleg Vashkevich <ugluk@tut.by>
2023-03-25 17:55:20 +0300

    ch27:TypeOf,ValueOf

--- ch27_reflection/main.go
+++ ch27_reflection/main.go
@@ -1,26 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"reflect"
-	"strings"
 )
+
+func getTypePath(t reflect.Type) (path string) {
+	path = t.PkgPath()
+	if path == "" {
+		path = "(built-in)"
+	}
+	return
+}
 
 func printDetails(values ...interface{}) {
 	for _, elem := range values {
-		fieldDetails := []string{}
 		elemType := reflect.TypeOf(elem)
+		Printfln("Name: %v, PkgPath: %v, Kind: %v",
+			elemType.Name(), getTypePath(elemType), elemType.Kind())
+
 		elemValue := reflect.ValueOf(elem)
-		if elemType.Kind() == reflect.Struct {
-			for i := 0; i < elemType.NumField(); i++ {
-				fieldName := elemType.Field(i).Name
-				fieldVal := elemValue.Field(i)
-				fieldDetails = append(fieldDetails,
-					fmt.Sprintf("%v: %v", fieldName, fieldVal))
+		switch elemValue.Kind() {
+		case reflect.Bool:
+			var val bool = elemValue.Bool()
+			Printfln("Bool: %v", val)
+		case reflect.Int:
+			var val int64 = elemValue.Int()
+			Printfln("Int: %v", val)
+		case reflect.Float32, reflect.Float64:
+			var val float64 = elemValue.Float()
+			Printfln("Float: %v", val)
+		case reflect.String:
+			var val string = elemValue.String()
+			Printfln("String: %v", val)
+		case reflect.Ptr:
+			var val reflect.Value = elemValue.Elem()
+			if val.Kind() == reflect.Int {
+				Printfln("Pointer to Int: %v", val.Int())
 			}
-			Printfln("%v: %v", elemType.Name(), strings.Join(fieldDetails, ", "))
-		} else {
-			Printfln("%v: %v", elemType.Name(), elemValue)
+		default:
+			Printfln("Other: %v", elemValue.String())
 		}
 
 	}
@@ -36,7 +54,10 @@ func main() {
 	product := Product{
 		Name: "Kayak", Category: "Watersports", Price: 279,
 	}
-	customer := Customer{Name: "Alice", City: "New York"}
-	payment := Payment{Currency: "USD", Amount: 100.50}
-	printDetails(product, customer, payment, 10, true)
+	//customer := Customer{Name: "Alice", City: "New York"}
+	//payment := Payment{Currency: "USD", Amount: 100.50}
+	//printDetails(product, customer, payment, 10, true)
+	number := 100
+	printDetails(true, 10, 23.30, "Alice", &number, product)
+
 }
