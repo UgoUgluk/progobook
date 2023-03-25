8c4a046f449e2a130d1d54aa06c01804c6758de9
Oleg Vashkevich <ugluk@tut.by>
2023-03-25 17:51:04 +0300

    ch27: use reflect

--- ch27_reflection/main.go
+++ ch27_reflection/main.go
@@ -1,14 +1,42 @@
 package main
 
-func printDetails(values ...Product) {
+import (
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+func printDetails(values ...interface{}) {
 	for _, elem := range values {
-		Printfln("Product: Name: %v, Category: %v, Price: %v",
-			elem.Name, elem.Category, elem.Price)
+		fieldDetails := []string{}
+		elemType := reflect.TypeOf(elem)
+		elemValue := reflect.ValueOf(elem)
+		if elemType.Kind() == reflect.Struct {
+			for i := 0; i < elemType.NumField(); i++ {
+				fieldName := elemType.Field(i).Name
+				fieldVal := elemValue.Field(i)
+				fieldDetails = append(fieldDetails,
+					fmt.Sprintf("%v: %v", fieldName, fieldVal))
+			}
+			Printfln("%v: %v", elemType.Name(), strings.Join(fieldDetails, ", "))
+		} else {
+			Printfln("%v: %v", elemType.Name(), elemValue)
+		}
+
 	}
 }
+
+// Payment present a payment
+type Payment struct {
+	Currency string
+	Amount   float64
+}
+
 func main() {
 	product := Product{
 		Name: "Kayak", Category: "Watersports", Price: 279,
 	}
-	printDetails(product)
+	customer := Customer{Name: "Alice", City: "New York"}
+	payment := Payment{Currency: "USD", Amount: 100.50}
+	printDetails(product, customer, payment, 10, true)
 }
