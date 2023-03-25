f4278627c2dd09ba5f1a68ef49c1018390069af5
Oleg Vashkevich <ugluk@tut.by>
2023-03-25 18:09:51 +0300

    ch27:change value, compare

--- ch27_reflection/main.go
+++ ch27_reflection/main.go
@@ -2,44 +2,81 @@ package main
 
 import (
 	"reflect"
+	"strings"
 )
 
-var intPtrType = reflect.TypeOf((*int)(nil))
-var byteSliceType = reflect.TypeOf([]byte(nil))
-
-func printDetails(values ...interface{}) {
+func selectValue(data interface{}, index int) (result interface{}) {
+	dataVal := reflect.ValueOf(data)
+	if dataVal.Kind() == reflect.Slice {
+		result = dataVal.Index(index).Interface()
+	}
+	return
+}
+func incrementOrUpper(values ...interface{}) {
 	for _, elem := range values {
 		elemValue := reflect.ValueOf(elem)
-		elemType := reflect.TypeOf(elem)
-		if elemType == intPtrType {
-			Printfln("Pointer to Int: %v", elemValue.Elem().Int())
-		} else if elemType == byteSliceType {
-			Printfln("Byte slice: %v", elemValue.Bytes())
-		} else {
+		if elemValue.CanSet() {
 			switch elemValue.Kind() {
-			case reflect.Bool:
-				var val bool = elemValue.Bool()
-				Printfln("Bool: %v", val)
 			case reflect.Int:
-				var val int64 = elemValue.Int()
-				Printfln("Int: %v", val)
-			case reflect.Float32, reflect.Float64:
-				var val float64 = elemValue.Float()
-				Printfln("Float: %v", val)
+				elemValue.SetInt(elemValue.Int() + 1)
 			case reflect.String:
-				var val string = elemValue.String()
-				Printfln("String: %v", val)
-			default:
-				Printfln("Other: %v", elemValue.String())
+				elemValue.SetString(strings.ToUpper(
+					elemValue.String()))
 			}
+			Printfln("Modified Value: %v", elemValue)
+		} else {
+			Printfln("Cannot set %v: %v", elemValue.Kind(),
+				elemValue)
 		}
 	}
 }
-func main() {
-	product := Product{
-		Name: "Kayak", Category: "Watersports", Price: 279,
+
+func setAll(src interface{}, targets ...interface{}) {
+	srcVal := reflect.ValueOf(src)
+	for _, target := range targets {
+		targetVal := reflect.ValueOf(target)
+		if targetVal.Kind() == reflect.Ptr &&
+			targetVal.Elem().Type() == srcVal.Type() &&
+			targetVal.Elem().CanSet() {
+			targetVal.Elem().Set(srcVal)
+		}
 	}
-	number := 100
-	slice := []byte("Alice")
-	printDetails(true, 10, 23.30, "Alice", &number, product, slice)
+}
+
+func contains(slice interface{}, target interface{}) (found bool) {
+	sliceVal := reflect.ValueOf(slice)
+	if sliceVal.Kind() == reflect.Slice {
+		for i := 0; i < sliceVal.Len(); i++ {
+			if reflect.DeepEqual(sliceVal.Index(i).Interface(), target) {
+				found = true
+			}
+		}
+	}
+
+	return
+}
+
+func main() {
+	//names := []string{"Alice", "Bob", "Charlie"}
+	//val := selectValue(names, 1).(string)
+	//Printfln("Selected: %v", val)
+	name := "Alice"
+	price := 279
+	city := "London"
+	incrementOrUpper(name, price, city)
+	for _, val := range []interface{}{name, price, city} {
+		Printfln("Value: %v", val)
+	}
+
+	setAll("New String", &name, &price, &city)
+	setAll(10, &name, &price, &city)
+	for _, val := range []interface{}{name, price, city} {
+		Printfln("Value: %v", val)
+	}
+
+	citiesSlice := []string{"Paris", "Rome", "London"}
+	Printfln("Found #1: %v", contains(citiesSlice, city))
+	sliceOfSlices := [][]string{citiesSlice, {"First", "Second", "Third"}}
+	Printfln("Found #2: %v", contains(sliceOfSlices, citiesSlice))
+
 }
