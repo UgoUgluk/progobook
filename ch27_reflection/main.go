7a5ed8bee7fecc5f7ee130096d869c435863c8ba
Oleg Vashkevich <ugluk@tut.by>
2023-03-25 18:00:23 +0300

    ch27: type definition

--- ch27_reflection/main.go
+++ ch27_reflection/main.go
@@ -4,60 +4,42 @@ import (
 	"reflect"
 )
 
-func getTypePath(t reflect.Type) (path string) {
-	path = t.PkgPath()
-	if path == "" {
-		path = "(built-in)"
-	}
-	return
-}
+var intPtrType = reflect.TypeOf((*int)(nil))
+var byteSliceType = reflect.TypeOf([]byte(nil))
 
 func printDetails(values ...interface{}) {
 	for _, elem := range values {
-		elemType := reflect.TypeOf(elem)
-		Printfln("Name: %v, PkgPath: %v, Kind: %v",
-			elemType.Name(), getTypePath(elemType), elemType.Kind())
-
 		elemValue := reflect.ValueOf(elem)
-		switch elemValue.Kind() {
-		case reflect.Bool:
-			var val bool = elemValue.Bool()
-			Printfln("Bool: %v", val)
-		case reflect.Int:
-			var val int64 = elemValue.Int()
-			Printfln("Int: %v", val)
-		case reflect.Float32, reflect.Float64:
-			var val float64 = elemValue.Float()
-			Printfln("Float: %v", val)
-		case reflect.String:
-			var val string = elemValue.String()
-			Printfln("String: %v", val)
-		case reflect.Ptr:
-			var val reflect.Value = elemValue.Elem()
-			if val.Kind() == reflect.Int {
-				Printfln("Pointer to Int: %v", val.Int())
+		elemType := reflect.TypeOf(elem)
+		if elemType == intPtrType {
+			Printfln("Pointer to Int: %v", elemValue.Elem().Int())
+		} else if elemType == byteSliceType {
+			Printfln("Byte slice: %v", elemValue.Bytes())
+		} else {
+			switch elemValue.Kind() {
+			case reflect.Bool:
+				var val bool = elemValue.Bool()
+				Printfln("Bool: %v", val)
+			case reflect.Int:
+				var val int64 = elemValue.Int()
+				Printfln("Int: %v", val)
+			case reflect.Float32, reflect.Float64:
+				var val float64 = elemValue.Float()
+				Printfln("Float: %v", val)
+			case reflect.String:
+				var val string = elemValue.String()
+				Printfln("String: %v", val)
+			default:
+				Printfln("Other: %v", elemValue.String())
 			}
-		default:
-			Printfln("Other: %v", elemValue.String())
 		}
-
 	}
 }
-
-// Payment present a payment
-type Payment struct {
-	Currency string
-	Amount   float64
-}
-
 func main() {
 	product := Product{
 		Name: "Kayak", Category: "Watersports", Price: 279,
 	}
-	//customer := Customer{Name: "Alice", City: "New York"}
-	//payment := Payment{Currency: "USD", Amount: 100.50}
-	//printDetails(product, customer, payment, 10, true)
 	number := 100
-	printDetails(true, 10, 23.30, "Alice", &number, product)
-
+	slice := []byte("Alice")
+	printDetails(true, 10, 23.30, "Alice", &number, product, slice)
 }
