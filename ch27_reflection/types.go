369a1fa30691b85fa1fe7114f4d2e9d1f270a140
Oleg Vashkevich <ugluk@tut.by>
2023-03-25 17:43:38 +0300

    ch27: start

---
+++ ch27_reflection/types.go
@@ -0,0 +1,12 @@
+package main
+
+//Product present a product
+type Product struct {
+	Name, Category string
+	Price          float64
+}
+
+//Customer present a customer
+type Customer struct {
+	Name, City string
+}
