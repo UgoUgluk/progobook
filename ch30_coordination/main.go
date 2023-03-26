25a1ee8318934d9bd40bba7cde896e7e240af56c
Oleg Vashkevich <ugluk@tut.by>
2023-03-26 12:53:14 +0300

    ch30: WaitGroup, Mutex, RWMutex

--- ch30_coordination/main.go
+++ ch30_coordination/main.go
@@ -1,12 +1,64 @@
 package main
 
-func doSum(count int, val *int) {
-	for i := 0; i < count; i++ {
-		*val++
+import (
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+var waitGroup = sync.WaitGroup{}
+var mutex = sync.Mutex{}
+var rwmutex = sync.RWMutex{}
+
+var squares = map[int]int{}
+
+func calculateSquares(max, iterations int) {
+	for i := 0; i < iterations; i++ {
+		val := rand.Intn(max)
+		rwmutex.RLock()
+		square, ok := squares[val]
+		rwmutex.RUnlock()
+		if ok {
+			Printfln("Cached value: %v = %v", val, square)
+		} else {
+			rwmutex.Lock()
+			if _, ok := squares[val]; !ok {
+				squares[val] = int(math.Pow(float64(val), 2))
+				Printfln("Added value: %v = %v", val, squares[val])
+			}
+			rwmutex.Unlock()
+		}
 	}
+	waitGroup.Done()
+}
+
+func doSum(count int, val *int) {
+	time.Sleep(time.Second)
+	for i := 0; i < count; i++ {
+		mutex.Lock()
+		*val++
+		mutex.Unlock()
+	}
+	waitGroup.Done()
 }
 func main() {
 	counter := 0
-	doSum(5000, &counter)
+
+	numRoutines := 3
+	waitGroup.Add(numRoutines)
+	for i := 0; i < numRoutines; i++ {
+		go doSum(5000, &counter)
+	}
+	waitGroup.Wait()
 	Printfln("Total: %v", counter)
+
+	rand.Seed(time.Now().UnixNano())
+	waitGroup.Add(numRoutines)
+	for i := 0; i < numRoutines; i++ {
+		go calculateSquares(10, 5)
+	}
+	waitGroup.Wait()
+	Printfln("Cached values: %v", len(squares))
+
 }
