780502ef8cf57ca97c20922d19380e474e11ece6
Oleg Vashkevich <ugluk@tut.by>
2023-03-26 12:56:36 +0300

    ch30: NewCond

--- ch30_coordination/main.go
+++ ch30_coordination/main.go
@@ -8,57 +8,45 @@ import (
 )
 
 var waitGroup = sync.WaitGroup{}
-var mutex = sync.Mutex{}
 var rwmutex = sync.RWMutex{}
-
+var readyCond = sync.NewCond(rwmutex.RLocker())
 var squares = map[int]int{}
 
-func calculateSquares(max, iterations int) {
+func generateSquares(max int) {
+	rwmutex.Lock()
+	Printfln("Generating data...")
+	for val := 0; val < max; val++ {
+		squares[val] = int(math.Pow(float64(val), 2))
+	}
+	rwmutex.Unlock()
+	Printfln("Broadcasting condition")
+	readyCond.Broadcast()
+	waitGroup.Done()
+}
+func readSquares(id, max, iterations int) {
+	readyCond.L.Lock()
+	for len(squares) == 0 {
+		readyCond.Wait()
+	}
 	for i := 0; i < iterations; i++ {
-		val := rand.Intn(max)
-		rwmutex.RLock()
-		square, ok := squares[val]
-		rwmutex.RUnlock()
-		if ok {
-			Printfln("Cached value: %v = %v", val, square)
-		} else {
-			rwmutex.Lock()
-			if _, ok := squares[val]; !ok {
-				squares[val] = int(math.Pow(float64(val), 2))
-				Printfln("Added value: %v = %v", val, squares[val])
-			}
-			rwmutex.Unlock()
-		}
+		key := rand.Intn(max)
+		Printfln("#%v Read value: %v = %v", id, key,
+			squares[key])
+		time.Sleep(time.Millisecond * 100)
 	}
+	readyCond.L.Unlock()
 	waitGroup.Done()
 }
 
-func doSum(count int, val *int) {
-	time.Sleep(time.Second)
-	for i := 0; i < count; i++ {
-		mutex.Lock()
-		*val++
-		mutex.Unlock()
-	}
-	waitGroup.Done()
-}
 func main() {
-	counter := 0
-
-	numRoutines := 3
-	waitGroup.Add(numRoutines)
-	for i := 0; i < numRoutines; i++ {
-		go doSum(5000, &counter)
-	}
-	waitGroup.Wait()
-	Printfln("Total: %v", counter)
-
 	rand.Seed(time.Now().UnixNano())
+	numRoutines := 2
 	waitGroup.Add(numRoutines)
 	for i := 0; i < numRoutines; i++ {
-		go calculateSquares(10, 5)
+		go readSquares(i, 10, 5)
 	}
+	waitGroup.Add(1)
+	go generateSquares(10)
 	waitGroup.Wait()
 	Printfln("Cached values: %v", len(squares))
-
 }
